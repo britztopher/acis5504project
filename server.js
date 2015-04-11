3e582b77087b46ec63371f6f8e8b519ed3829294
EC2 Default User <ec2-user@ip-172-31-38-136.ec2.internal>
2015-04-11 23:17:14 +0000

    took out some openshift stuff and switched ports and env vars to be more generic

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 #!/bin/env node
-//  OpenShift sample Node application
+var fs = require('fs');
 var express = require('express');
-var fs      = require('fs');
 var path = require('path');
 
 
@@ -23,8 +22,8 @@ var SampleApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        self.ipaddress = process.env.NODEJS_IP || 'localhost';
+        self.port  = process.env.NODEJS_PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
